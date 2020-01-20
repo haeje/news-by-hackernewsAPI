3d2c85e1f516e26dcc4dcb89792f5f009a3a5e66
haja <parkhaeje@gmail.com>
2020-01-20 21:33:15 +0900

    toolbar css

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,17 +1,29 @@
 !<template>
-  <div>
-      <router-link to="/news">news</router-link>
-      <router-link to="/ask">ask</router-link>
+  <div class="header">
+      <router-link to="/news">news</router-link> |
+      <router-link to="/ask">ask</router-link> |
       <router-link to="/jobs">jobs</router-link>
   </div>
 </template>
-
+~
 <script>
 export default {
 
 }
 </script>
 
-<style>
+<style scoped>
+.header{
+  color : white;
+  background-color: #42b883;
+  display: flex;
+  padding : 8px;
+}
+.header .router-link-exact-active  {
+  color : #35495e;
+}
 
+.header a{
+  color : white;
+}
 </style>
