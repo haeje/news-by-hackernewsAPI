8f5b656205243ac41d3ff29f2321ac70bd753d83
haja <parkhaeje@gmail.com>
2020-01-20 21:40:57 +0900

    router history mode 적용

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router';
 import newsView from '../views/news.vue';
 import askView from '../views/ask.vue';
 import jobView from '../views/jobs.vue';
+import itemView from '../views/items.vue';
+import userView from '../views/user.vue';
 
 Vue.use(VueRouter);
 
@@ -23,10 +25,18 @@ const routes = [
     {
         path : '/jobs',
         component : jobView
+    },
+    {
+        path : '/items',
+        component : itemView
+    },
+    {
+        path : '/user',
+        component : userView
     }
 ];
 
 export const router = new VueRouter({
-    // mode : 'history',
+    mode : 'history',
     routes
 })
